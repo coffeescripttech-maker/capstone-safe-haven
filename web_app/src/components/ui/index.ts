14ebe975c950f34bc45d7cde9489ff4703f16113
xedter20 <dextermiranda441@gmail.com>
2026-05-31 17:10:55 +0800

    fixes

--- web_app/src/components/ui/index.ts
+++ web_app/src/components/ui/index.ts
@@ -2,4 +2,11 @@
 export { Button } from './Button';
 export { Alert } from './Alert';
 export { Input, Textarea } from './Input';
-export { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from './Card';
+
+// Card components
+export { Card } from './Card';
+export { CardContent } from './Card';
+export { CardHeader } from './Card';
+export { CardTitle } from './Card';
+export { CardDescription } from './Card';
+export { CardFooter } from './Card';
