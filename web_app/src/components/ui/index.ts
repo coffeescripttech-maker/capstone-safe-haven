b420f863419674d08490d38e42a8e89eb4f83aed
xedter20 <dextermiranda441@gmail.com>
2026-05-31 17:08:28 +0800

    fixes

--- web_app/src/components/ui/index.ts
+++ web_app/src/components/ui/index.ts
@@ -1,12 +1,5 @@
-// SafeHaven UI Components - Centralized Exportsss
+// SafeHaven UI Components - Centralized Exports
 export { Button } from './Button';
 export { Alert } from './Alert';
 export { Input, Textarea } from './Input';
-export { 
-  Card, 
-  CardContent, 
-  CardHeader, 
-  CardTitle, 
-  CardDescription, 
-  CardFooter 
-} from './Card';
+export { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from './Card';
